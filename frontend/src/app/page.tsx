0d88907b11cacd477a952073c04499ff00c41d25
JorgeVanco <jorge.vanco.sampedro@gmail.com>
2025-08-04 23:13:54 +0200

    feat: google login

--- frontend/src/app/page.tsx
+++ frontend/src/app/page.tsx
@@ -1,8 +1,21 @@
-import Image from "next/image";
+"use client";
+
+import { signIn, signOut, useSession } from "next-auth/react";
 import Calendar from "@/components/Calendar";
 export default function Home() {
+
+  const { data: session } = useSession();
+
   return (
     <div>
+      {session ? (
+        <>
+          <p>Signed in as {session.user?.email}</p>
+          <button onClick={() => signOut()}>Sign out</button>
+        </>
+      ) : (
+        <button onClick={() => signIn('google')}>Sign in with Google</button>
+      )}
       <main className="p-4">
         <Calendar />
       </main>
