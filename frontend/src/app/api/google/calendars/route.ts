8d49eaba1c4e842d043f8891f1a4e6496c8d8862
JorgeVanco <jorge.vanco.sampedro@gmail.com>
2025-08-05 17:04:25 +0200

    chore: change color hue of calendar colors to match a bit more correct colors

--- frontend/src/app/api/google/calendars/route.ts
+++ frontend/src/app/api/google/calendars/route.ts
@@ -1,7 +1,8 @@
 import { getServerSession } from 'next-auth';
-import { authOptions } from '@/lib/authOptions';  // <--- this now works
+import { authOptions } from '@/lib/authOptions';
 import { google } from 'googleapis';
 import { NextRequest, NextResponse } from 'next/server';
+import { adjustGoogleCalendarColor } from '@/lib/utils';
 
 export async function GET(req: NextRequest) {
     const session = await getServerSession(authOptions);
@@ -17,7 +18,8 @@ export async function GET(req: NextRequest) {
 
     try {
         const res = await calendar.calendarList.list();
-        return NextResponse.json(res.data);
+        const calendars = { items: res.data.items?.map((calendar) => ({ ...calendar, backgroundColor: adjustGoogleCalendarColor(calendar.backgroundColor as string) })) };
+        return NextResponse.json(calendars);
     } catch (error) {
         console.error('Google Calendar API Error:', error);
         return NextResponse.json({ error: 'Failed to fetch calendars' }, { status: 500 });
